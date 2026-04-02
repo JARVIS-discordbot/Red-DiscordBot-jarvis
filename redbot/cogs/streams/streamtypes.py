d9fae7f77d60bd5db07bed63e8378fd2cd9c3dc1
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2026-04-02 20:12:38 +0100

    Fix encoding issue in get_video_ids_from_feed
    change root = ET.fromstring(feed) to root = ET.fromstring(feed.encode("utf-8").strip())
    
    line 59 in streamstypes.py

--- redbot/cogs/streams/streamtypes.py
+++ redbot/cogs/streams/streamtypes.py
@@ -56,7 +56,7 @@ def rnd(url):
 
 
 def get_video_ids_from_feed(feed):
-    root = ET.fromstring(feed)
+    root = ET.fromstring(feed.encode("utf-8").strip())
     rss_video_ids = []
     for child in root.iter("{http://www.w3.org/2005/Atom}entry"):
         for i in child.iter("{http://www.youtube.com/xml/schemas/2015}videoId"):
